855ddc8085ba875eb1239316891335650f0d0395
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-04-06 23:22:14 +0530

    The validation of email

--- task-7/src/main.rs
+++ task-7/src/main.rs
@@ -1,7 +1,8 @@
+extern crate regex;
 use regex::Regex;
 fn main()
 {
-let re = Regex::new( re r"^([a-zA-Z\d]+)@(a-zA-Z\d]+)\.([a-z]{2,8})$").unwrap();
+let re = Regex::new(r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$").unwrap();
 assert!(re.is_match("good@email.com"));
 println!("IS A VALID EMAIL");
 }
