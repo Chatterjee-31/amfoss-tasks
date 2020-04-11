523d89c6a2773d1882399daded66ac571a42ac19
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-04-12 00:21:02 +0530

    WIP

--- Task-6/twitter.go
+++ Task-6/twitter.go
@@ -7,29 +7,29 @@ import (
         "flag"
         "os"
 )
-function main(){
+func main(){
 
-tweetby:=flag.string("tweeterhandle",BhattacharDeb,"handler of user")
-flag.Parse()
-config := oauth1.NewConfig("WxQPG0QcXsMbhnIChleuSjvhq", "K6qsXuQrSUpIwW0UrgtnMOCi73qCE6XQyoroTExsHhqsoqbJIu")
-token := oauth1.NewToken("1248636080800186369-4El0HFkpO1SvpbYT6fu5ovSTQZ7JBW", "UDiwignmneNx2peVTEUZdUYQTRePPOvOuxaaYFq3u0od8")
+         tweetby:=flag.string("tweeterhandle",BhattacharDeb,"handler of user")
+         flag.Parse()
+         config := oauth1.NewConfig("WxQPG0QcXsMbhnIChleuSjvhq", "K6qsXuQrSUpIwW0UrgtnMOCi73qCE6XQyoroTExsHhqsoqbJIu")
+         token := oauth1.NewToken("1248636080800186369-4El0HFkpO1SvpbYT6fu5ovSTQZ7JBW", "UDiwignmneNx2peVTEUZdUYQTRePPOvOuxaaYFq3u0od8")
 
-httpClient := config.Client(oauth1.NoContext, token)
-client := twitter.NewClient(httpClient)
+         httpClient := config.Client(oauth1.NoContext, token)
+         client := twitter.NewClient(httpClient)
 
 
 
-params := &twitter.FollowersListParam{
-ScreenName: *tweetby,}
-followers, resp, err := client.Followers.List(FollowerListParams)
-fmt.Println(resp, err)
+         params := &twitter.FollowersListParam{
+         ScreenName: *tweetby,}
+         followers, resp, err := client.Followers.List(FollowerListParams)
+         fmt.Println(resp, err)
 
-f,err := os.Create("twitterhandles.txt")}
+         f,err := os.Create("twitterhandles.txt")}
 
-var count int = 0;
-for _, follower := range followers{
-	count++
-	f.WriteString("\n" + followers.ScreenName)}
+         var count int = 0;
+         for _, follower := range followers{
+	 count++
+	 f.WriteString("\n" + followers.ScreenName)}
 	
-	f.WriteString(fmt.printf("\n", count))
-f.Close()
+	 f.WriteString(fmt.printf("\n", count))
+         f.Close()
